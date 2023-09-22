015ded6358a030c9d460b31b3f97a636015975b8
YasinBG <116117219+YasinBG@users.noreply.github.com>
2023-09-22 16:13:01 +0300

    json belgeden veri alindi

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,15 +2,37 @@
   <div class="text-2xl text-green-500">
     test
   </div>
+
+  <div
+   v-for="destination in destinations" :key="destination.id">
+
+   <p>{{ destination.id }}</p>
+
+   <h1>{{ destination.title }}</h1>
+   
+   <p>{{ destination.body }}</p>
+
+   <img class="w-96 h-96 mx-auto" :src="destination.image" alt="">
+
+
+   
+</div>
 </template>
 
 <script>
 // @ is an alias to /src
-
+import sourceData from '../components/data.json'
+import { ref } from 'vue'
 export default {
   name: 'HomeView',
   components: {
     
+  },
+  setup(){
+   const destinations =ref(sourceData.destinations)
+    return{destinations
+
+    }
   }
 }
 </script>
