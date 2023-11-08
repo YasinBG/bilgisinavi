77d259426a57eb95d641f409e868ee5b9ddc0570
YasinBG <116117219+YasinBG@users.noreply.github.com>
2023-11-08 14:11:31 +0300

    update

--- src/views/TestView.vue
+++ src/views/TestView.vue
@@ -1,72 +1,70 @@
 <template>
-    <div class="flex flex-col">
-      <input
-        class="w-60 h-12 border-4 border-black rounded mx-auto"
-        v-model="newTask"
-        @keyup.enter="addTask"
-        placeholder="  Görev ekle..."
-      />
-      <ul class="my-6 flex flex-col">
-      
-        <li class="flex text-start  my-2 font-semibold " v-for="(task, index) in tasks" :key="index">
-          <span class="mx-6 " :style="{ textDecoration: task.completed ? 'line-through' : 'none' }">
-            {{ task.title }}
-          </span>
-          
-          <div class="fixed ml-80 flex ">
-            <button class=" rounded w-8" @click="completeTask(index)">
-            <input class=" border w-6 h-6 cursor-pointer" type="checkbox" />
-          </button>
-            <button class="border rounded bg-red-300 ml-6 text-white w-12 h-6" @click="deleteTask(index)">
+  <div class="">
+    <input
+      class="w-60 h-12 border-4 border-black rounded mx-auto"
+      v-model="newTask"
+      @keyup.enter="addTask"
+      placeholder="  Görev ekle..."
+    />
+    <ul class="my-6 flex flex-col">
+      <li class="flex text-start  my-2 font-semibold " v-for="(task, index) in tasks" :key="index">
+        <input class="border rounded w-6 h-6" type="checkbox" @change="completeTask(index)" :checked="task.completed" />
+        <span class="mx-2" :style="{ textDecoration: task.completed ? 'line-through' : 'none' }">
+          {{ truncateText(task.title, 20) }}
+        </span>
+        <div class="fixed ml-40 flex">
+          <button class="border rounded bg-red-300 ml-6 text-white w-12 h-6" @click="deleteTask(index)">
             Sil
           </button>
-          </div>
-          
-        </li>
-      </ul>
-      <div :style="{ backgroundColor: color }" class="renk">
-        <button @click="changeColor" class="border bg-green-200 text-center rounded w-32 h-10">
-          Renk Değiştir
-        </button>
-      </div>
-    </div>
-  </template>
-  
-  <script setup>
-  import { ref } from 'vue';
-  
-  const tasks = ref([
-    { title: 'Örnek Görev 1', completed: false },
-    { title: 'Örnek Görev 2', completed: true },
-  ]);
-  
-  const newTask = ref('');
-  
-  const addTask = () => {
-    tasks.value.push({ title: newTask.value, completed: false });
-    newTask.value = '';
-  };
-  
-  const deleteTask = (index) => {
-    tasks.value.splice(index, 1);
-  };
-  
-  const completeTask = (index) => {
-    tasks.value[index].completed = true;
-  };
-  
-  const color = ref('lightblue');
-  
-  const changeColor = () => {
-    const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
-    color.value = randomColor;
-  };
-  </script>
-  
-  <style scoped>
-    .renk {
-      width: 200px;
-      height: 200px;
-    }
-  </style>
-  
+        </div>
+      </li>
+    </ul>
+    <button class="border rounded bg-green-300  ml-6 text-white w-12 h-6" @click="recoverTask">
+      Geri Al
+    </button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const tasks = ref([
+  { title: 'Örnek Görev 1', completed: false },
+  { title: 'Örnek Görev 2', completed: true },
+]);
+
+const newTask = ref('');
+
+const deletedTasks = ref([]);
+
+const addTask = () => {
+  tasks.value.push({ title: newTask.value, completed: false });
+  newTask.value = '';
+};
+
+const deleteTask = (index) => {
+  const deletedTask = tasks.value.splice(index, 1)[0];
+  deletedTasks.value.unshift(deletedTask);
+};
+
+const recoverTask = () => {
+  if (deletedTasks.value.length > 0) {
+    const recoveredTask = deletedTasks.value.shift();
+    tasks.value.push(recoveredTask);
+  }
+};
+
+const completeTask = (index) => {
+  tasks.value[index].completed = !tasks.value[index].completed;
+};
+
+const truncateText = (text, length) => {
+  if (text.length > length) {
+    return text.substring(0, length) + '...';
+  }
+  return text;
+};
+</script>
+
+<style scoped>
+</style>
